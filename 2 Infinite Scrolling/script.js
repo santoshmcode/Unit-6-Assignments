3063dbc00d5c66d8965c45e723a909ef6136a656
Santosh Mane <santoshmane619@gmail.com>
2021-11-22 19:14:55 +0530

    completed Infinite Scrolling Assignment

--- 2 Infinite Scrolling/script.js
+++ 2 Infinite Scrolling/script.js
@@ -1,6 +1,21 @@
 console.log("connected");
 
 const container = document.getElementById("sub-container");
+const scrollTop = document.getElementById("scroll-top");
+const clientHeight = document.getElementById("client-height");
+const scrollHeight = document.getElementById("scroll-height");
+const getBoundingClientRect = document.getElementById(
+    "get-bounding-client-rect"
+);
+
+const data = container.getBoundingClientRect();
+const data2 = container.getClientRects();
+
+scrollTop.innerHTML = container.scrollTop;
+clientHeight.innerHTML = container.clientHeight;
+scrollHeight.innerHTML = container.scrollHeight;
+getBoundingClientRect.innerHTML = JSON.stringify(data, undefined, 4);
+
 let studentCount = 1;
 
 const addMasaiStudents = () => {
@@ -13,5 +28,19 @@ const addMasaiStudents = () => {
         studentCount++;
     }
 };
+
+container.addEventListener("scroll", () => {
+    scrollTop.innerHTML = container.scrollTop;
+    clientHeight.innerHTML = container.clientHeight;
+    scrollHeight.innerHTML = container.scrollHeight;
+
+    let SCROLL_TOP = container.scrollTop;
+    let CLIENT_HEIGHT = container.clientHeight;
+    let SCROLL_HEIGHT = container.scrollHeight;
+
+    if (SCROLL_TOP + CLIENT_HEIGHT === SCROLL_HEIGHT) {
+        addMasaiStudents();
+    }
+});
 
 addMasaiStudents();
