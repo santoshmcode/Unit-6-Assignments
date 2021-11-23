c6b6a3b59da4470dee4527c76a1014b0fdb47954
Santosh Mane <santoshmane619@gmail.com>
2021-11-23 23:30:59 +0530

    created ui of first page

--- 3 Marvel Search Engine/script.js
+++ 3 Marvel Search Engine/script.js
@@ -1,1 +1,10 @@
 console.log("connected");
+
+const mainContainer = document.getElementById("main-container");
+const searchBtn = document.getElementById("search-btn");
+const resultContainer = document.getElementById("results-container");
+
+searchBtn.addEventListener("click", () => {
+    mainContainer.style.marginTop = "calc(-100vh + 40px)";
+    resultContainer.style.transform = "translateY(-100%)";
+});
