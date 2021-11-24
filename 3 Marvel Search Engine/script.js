cfae69a9252f09b1fb0d53fca67c3fd1227a79a4
Santosh Mane <santoshmane619@gmail.com>
2021-11-24 21:26:49 +0530

    created comic details static page

--- 3 Marvel Search Engine/script.js
+++ 3 Marvel Search Engine/script.js
@@ -6,6 +6,9 @@ const resultContainer = document.getElementById("result-container");
 const input = document.getElementById("input");
 const searchResult = document.getElementById("input-box-next");
 const PUBLIC_KEY = "da35a1a7ef103683b22151bf08133393";
+const backBtn = document.getElementById("back");
+console.log("backBtn:", backBtn);
+
 var timeId = null;
 
 searchBtn.addEventListener("click", () => {
@@ -46,7 +49,7 @@ const fatchApi = async (query) => {
 // fatchApi("hulk");
 
 const populateSearchResult = (data) => {
-    console.log("data:", data);
+    // console.log("data:", data);
 
     let { results } = data;
 
@@ -54,14 +57,16 @@ const populateSearchResult = (data) => {
         const div = document.createElement("div");
         div.classList.add("p-results");
 
+        let { title, id } = results[i];
+        const titleDiv = document.createElement("p");
+
         div.addEventListener("click", () => {
             console.log("Hi");
             console.log("resultContainer", resultContainer);
             resultContainer.style.marginTop = "calc(-100vh + 20px)";
-        });
 
-        let { title, id } = results[i];
-        const titleDiv = document.createElement("p");
+            populateDetailData(id);
+        });
 
         titleDiv.innerHTML = title;
 
@@ -69,3 +74,36 @@ const populateSearchResult = (data) => {
         searchResult.appendChild(div);
     }
 };
+
+const populateDetailData = async (id) => {
+    console.log("called");
+
+    const ts = 1;
+    const hash = "507c10df9d5faa7703d50cdb83d2ba91";
+    const res = await fetch(
+        `https://gateway.marvel.com:443/v1/public/comics/${id}?ts=${ts}&apikey=${PUBLIC_KEY}&hash=${hash}`
+    );
+
+    const data = await res.json();
+    const results = data.data.results[0];
+    console.log("data:", data.data.results[0]);
+    const {
+        title,
+        thumbnail: { path },
+        creators: { items, available },
+        prices: { type, price },
+    } = results;
+    for (let i = 0; i < available; i++) {
+        console.log(items[i].role, items[i].name);
+    }
+    console.log("title, path:", title, `${path}/portrait_uncanny.jpg`);
+};
+
+backBtn.addEventListener("click", () => {
+    resultContainer.style.marginTop = "calc(100vh + 20px)";
+});
+
+// https://gateway.marvel.com:443/v1/public/comics/5645?apikey=
+
+// delete this line later
+resultContainer.style.marginTop = "calc(-100vh + 20px)";
