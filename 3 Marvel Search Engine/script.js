454f1c494f2d021ac70529a8ac47578c8401933e
Santosh Mane <santoshmane619@gmail.com>
2021-11-24 17:45:48 +0530

    done 1st part

--- 3 Marvel Search Engine/script.js
+++ 3 Marvel Search Engine/script.js
@@ -2,19 +2,34 @@ console.log("connected");
 
 const mainContainer = document.getElementById("main-container");
 const searchBtn = document.getElementById("search-btn");
-const resultContainer = document.getElementById("results-container");
+const resultContainer = document.getElementById("result-container");
 const input = document.getElementById("input");
+const searchResult = document.getElementById("input-box-next");
+const PUBLIC_KEY = "da35a1a7ef103683b22151bf08133393";
+var timeId = null;
 
 searchBtn.addEventListener("click", () => {
     // fatchApi();
 });
 
 input.addEventListener("keyup", (e) => {
-    let value = e.target;
-    // fatchApi(e.target.value);
+    let { value } = e.target;
+    value === "" ? (searchResult.innerHTML = null) : "";
+    searchResult.innerHTML = null;
+    let data = throttelFunction(value, 250);
 });
 
-const PUBLIC_KEY = "da35a1a7ef103683b22151bf08133393";
+function throttelFunction(query, delay) {
+    if (!timeId) {
+        timeId = setTimeout(() => {
+            fatchApi(query);
+            clearTimeout(timeId);
+            timeId = null;
+        }, delay);
+    } else {
+        return;
+    }
+}
 
 const fatchApi = async (query) => {
     console.log("called");
@@ -26,7 +41,31 @@ const fatchApi = async (query) => {
     );
 
     const data = await res.json();
-    console.log("data:", data.data);
-    return data.data;
+    populateSearchResult(data.data);
 };
-// fatchApi();
+// fatchApi("hulk");
+
+const populateSearchResult = (data) => {
+    console.log("data:", data);
+
+    let { results } = data;
+
+    for (let i = 0; i < results.length; i++) {
+        const div = document.createElement("div");
+        div.classList.add("p-results");
+
+        div.addEventListener("click", () => {
+            console.log("Hi");
+            console.log("resultContainer", resultContainer);
+            resultContainer.style.marginTop = "calc(-100vh + 20px)";
+        });
+
+        let { title, id } = results[i];
+        const titleDiv = document.createElement("p");
+
+        titleDiv.innerHTML = title;
+
+        div.appendChild(titleDiv);
+        searchResult.appendChild(div);
+    }
+};
