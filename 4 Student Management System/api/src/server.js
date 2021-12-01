b5fcddbd5f88aecf44c81c3b897ec94d63d93202
Santosh Mane <santoshmane619@gmail.com>
2021-12-01 23:33:00 +0530

    connected db to server

--- 4 Student Management System/api/src/server.js
+++ 4 Student Management System/api/src/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const connect = require("./config/db");
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Hello");
-});
-
-app.listen(process.env.PORT || 5599, () => {
-    console.log(`http://localhost:${process.env.PORT || 5599}`);
+app.listen(process.env.PORT || 5599, async () => {
+    try {
+        await connect();
+        console.log(`http://localhost:${process.env.PORT || 5599}`);
+    } catch (error) {
+        console.log(error.message);
+    }
 });
