85e58728c05f51267190a55bc86ddfdcd2150f9e
Santosh Mane <santoshmane619@gmail.com>
2021-12-02 00:19:38 +0530

    created model and routes for admin

--- 4 Student Management System/api/src/server.js
+++ 4 Student Management System/api/src/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const connect = require("./config/db");
+require("dotenv").config();
 const app = express();
+const AdminController = require("./controllers/admin.controller");
+
 app.use(express.json());
+app.use("/admin", AdminController);
 
 app.listen(process.env.PORT || 5599, async () => {
     try {
